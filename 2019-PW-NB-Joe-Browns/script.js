22970900b617ed053ad9adb11a82bf91438943ed
designdevelopmake <designdevelopmake@gmail.com>
2026-03-30 19:52:22 +0100

    Add files via upload

---
+++ 2019-PW-NB-Joe-Browns/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+//drop downs
+				$(document).ready(function() {
+					$(".dropDown1").click(function(){
+					$(this).next(".hiddenContent").slideToggle("slow");
+                    $(".dropDown1 .chevron-down").toggleClass("rtoate180--1");
+									
+					});
+				});
+
+});
